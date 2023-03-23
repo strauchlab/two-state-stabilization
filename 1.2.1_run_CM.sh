98aa1970089a0a0787b112c89560e6474f51b794
Karen J. Gonzalez <kg67392@uga.edu>
2023-03-23 11:50:59 -0400

    Add files via upload

---
+++ 1.2.1_run_CM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+$Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease \
+-database $Rosetta/main/database \
+-in:file:fasta INPUT.fasta \
+-parser:protocol 1.2.0_rosetta_comparative_modeling.xml \ 
+-relax:dualspace \
+-relax:jump_move true \
+-edensity:mapfile INPUT_map.map \ #cryoEM map
+-edensity:mapreso x \ # x= INPUT map resolution 
+-edensity::cryoem_scatterers \
+-beta \
+-hybridize:stage1_probability 1.0 \
+-ignore_unrecognized_res \
+-out:nstruct 5 \ #number of desired output structures
+
+ 
+ 
---
+++ 2.3_alanine_scanning.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$Rosetta/main/source/bin/cartesian_ddg.static.linuxgccrelease \
+-database $Rosetta/main/database \
+-s INPUT.pdb \ #trimeric structure as cartesian_ddg is not aware of symmetry
+-ddg:iterations 3 \
+-ddg::cartesian \
+-ddg::dump_pdbs false \
+-ddg:bbnbrs 1 \
+-beta_cart \
+-fa_max_dis 9.0 \
+-ddg:mut_file mut_file \ #mut_file contains the position to be mutated by alanine
+
+
