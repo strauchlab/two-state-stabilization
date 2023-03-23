1c317a78693d625839dae7ebfca075a1efa69dca
Karen J. Gonzalez <kg67392@uga.edu>
2023-03-23 12:19:41 -0400

    Create 1.1_relax_density.sh

---
+++ codes/1.1_relax_density.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+$Rosetta/main/source/bin/relax.linuxgccrelease \
+-database $Rosetta/main/database \
+-in:file:s INPUT_Monomer.pdb \ #the monomeric structure as symmetry is used to define the trimer
+-relax:fast \
+-relax:jump_move true \
+-edensity:mapfile INPUT_map.ccp4 \
+-edensity:mapreso x \ # x= INPUT map resolution 
+-edensity:fastdens_wt y \ # y= density weight according to map resolution. 
+-symmetry_definition INPUT_symmetry_file.symm \ #symmetry definition file
+-ignore_unrecognized_res \
+-out::nstruct 5 \ #number of desired output structures
+-ex1 -ex2 \
+ 
